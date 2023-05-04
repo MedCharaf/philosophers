768c4b2538cfb92919c0f0d541d891ee486240fb
Mohamed charaf Bouderraoui <mbouderr@c2r2p10.1337.ma>
2023-05-04 08:53:52 +0100

    fix mutex

--- main.c
+++ main.c
@@ -1,17 +1,24 @@
 #include "philo.h"
 #include <stdio.h>
 
+int incr =0;
+  pthread_mutex_t mutex;
 void* print_message() {
-      // Cast the void* argument to ThreadData*
-    printf("Thread \n");  // Print the thread's ID and message
-    sleep(3);
-    printf("ending thread\n");
+int i  = 0;
+    while(i <1000000){
+      pthread_mutex_lock(&mutex);
+        incr++;
+      pthread_mutex_unlock(&mutex);
+    i++;
+    }
+
     return NULL;
 }
 
 int main(int argc , char **argv){
 
 if(argc == 6 ){
+  
     //  int  times_philosopher_must_eat = 0;
     int num_of_philos = ft_atoi_v2(argv[1]);
 //      int time_to_die = ft_atoi_v2(argv[2]);
@@ -20,21 +27,15 @@ if(argc == 6 ){
 //    times_philosopher_must_eat = ft_atoi_v2(argv[5]);
    int i = 0;
     pthread_t threads[num_of_philos];
-
+    pthread_mutex_init(&mutex , NULL);
     i = 0;
     while(i < num_of_philos ){
-         if(pthread_create(&threads[i], NULL, &print_message,NULL));
+        pthread_create(&threads[i], NULL, &print_message,NULL);
+        pthread_join(threads[i], NULL);
          i++;
     }
-
-        for (int i = 0; i < num_of_philos; i++) {
-        pthread_join(threads[i], NULL);
-    }
-
-
-
-return 0;
-
+pthread_mutex_destroy(&mutex);
+    printf("%d\n", incr);
 }
 else 
 printf("check args\n");
